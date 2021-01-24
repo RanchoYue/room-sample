3560541b5b47dcd59bffd48e6fb6547e5bfc5413
yue <1263887632@qq.com>
2021-01-24 21:00:20 +0800

    update

--- app/src/main/java/com/example/room/models/User.kt
+++ app/src/main/java/com/example/room/models/User.kt
@@ -6,7 +6,6 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class User(
-
     @ColumnInfo(name = "first_name") val firstName: String?,
     @ColumnInfo(name = "last_name") val lastName: String?
 ) {
