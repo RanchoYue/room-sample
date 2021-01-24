6b1f2d954c51c419e3f2f944a5b4ae1f5008826a
yue <1263887632@qq.com>
2021-01-24 12:06:24 +0800

    init

---
+++ app/src/main/java/com/example/room/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.room
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+import com.example.room.database.AppDatabase
+import com.example.room.models.User
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val userDao = AppDatabase.getInstance(this).userDao()
+
+        val tom = User(0, "tom", "jack")
+        val tony = User(1, "tony", "white")
+
+        btn_insert.setOnClickListener {
+            userDao.insertAll(tom, tony)
+        }
+        btn_delete.setOnClickListener {
+            userDao.delete(tom)
+        }
+        btn_update.setOnClickListener {
+            val loadAllByIds = userDao.loadAllByIds(intArrayOf(1))
+            Log.d("yue_qf", "loadAllByIds: $loadAllByIds")
+        }
+        btn_query.setOnClickListener {
+            val user = userDao.findByName("tony", "white")
+            Log.d("yue_qf", "user: $user")
+        }
+    }
+
+}
